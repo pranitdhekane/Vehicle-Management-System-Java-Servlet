61031d14928a9699899572fa37e46d948a42f683
root <root@ip-172-31-4-167.ap-south-1.compute.internal>
2026-09-09 11:52:51 +0000

    bug fix

--- src/com/vehicle/DbConnection.java
+++ src/com/vehicle/DbConnection.java
@@ -1,20 +1,27 @@
 package com.vehicle;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnection {
-	
-	public Connection makeConnection() throws ClassNotFoundException, SQLException {
-		String dbDriver = "com.mysql.cj.jdbc.Driver";
-        String dbURL = "jdbc:mysql:// localhost:3306/";
-        // Database name to access
-        String dbName = "vehicle";
-        String dbUsername = "vehicleuser";
-        String dbPassword = "Vehicle@123";
-  
+
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+
+        String dbDriver = "com.mysql.cj.jdbc.Driver";
+        String dbURL = "jdbc:mysql://localhost:3306/";
+        String dbName = "demo";
+        String dbUsername = "pranit";
+        String dbPassword = "project";
+
         Class.forName(dbDriver);
-        Connection con = DriverManager.getConnection(dbURL + dbName,
-                                                     dbUsername, 
-                                                     dbPassword);
+
+        Connection con = DriverManager.getConnection(
+                dbURL + dbName,
+                dbUsername,
+                dbPassword
+        );
+
         return con;
-	}
+    }
 }
