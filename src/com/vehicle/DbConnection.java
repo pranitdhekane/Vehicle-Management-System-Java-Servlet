8544bd5a0cccf07fe7e22c8d37605ddc2123a44f
root <root@ip-172-31-4-167.ap-south-1.compute.internal>
2026-09-09 14:36:24 +0000

    Fix DB URL

--- src/com/vehicle/DbConnection.java
+++ src/com/vehicle/DbConnection.java
@@ -9,7 +9,7 @@ public class DbConnection {
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
 
         String dbDriver = "com.mysql.cj.jdbc.Driver";
-        String dbURL = "jdbc:mysql://localhost:3306/";
+        String dbURL = "jdbc:mysql://mvn-project.cjkyyo622kzj.ap-south-1.rds.amazonaws.com:3306/";
         String dbName = "demo";
         String dbUsername = "pranit";
         String dbPassword = "project";
